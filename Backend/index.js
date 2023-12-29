275cf95628a5c8f01efeda18981a966a4582fcac
Arnab199993 <103571005+Arnab199993@users.noreply.github.com>
2023-12-30 00:05:27 +0530

    added signup api in backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+require("./DataBase/Config");
+const users = require("./DataBase/Users");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("App is running");
+app.use(express.json());
+app.use(cors());
+app.get("/", async (req, res) => {
+  try {
+    const data = await users.find();
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post("/sign-up", async (req, res) => {
+  const data = new users(req.body);
+  const result = await data.save();
+  console.log(req.body);
+  console.log(result);
+  res.send(result);
 });
 app.listen(5000);
