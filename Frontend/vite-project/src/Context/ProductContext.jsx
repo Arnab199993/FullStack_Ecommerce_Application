14881ee8d008359e28295b18604ae87c0804c7b8
Arnab199993 <103571005+Arnab199993@users.noreply.github.com>
2024-01-20 19:37:41 +0530

    changes

---
+++ Frontend/vite-project/src/Context/ProductContext.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+const ProductContext = React.createContext(null);
+export const useProductContext = () => React.useContext(ProductContext);
+const BASE_URL = "http://localhost:5000";
+const ProductContextProvider = ({ children }) => {
+  const [product, setProduct] = useState([]);
+
+  const storedData = localStorage.getItem("token");
+  const fetchData = async () => {
+    try {
+      const res = await fetch(`${BASE_URL}/productsList`, {
+        headers: {
+          authorization: `bearer ${storedData}`,
+        },
+      });
+      const data = await res.json();
+      if (data?.length > 0) {
+        setProduct(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <ProductContext.Provider value={{ fetchData, product, setProduct }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+export default ProductContextProvider;
