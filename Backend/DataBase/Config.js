58e80e1b336fce53b8f1a432f2439c5330e52a02
Arnab199993 <103571005+Arnab199993@users.noreply.github.com>
2024-01-25 14:32:24 +0530

    connected mongo db compass with mongo db atlas

--- Backend/DataBase/Config.js
+++ Backend/DataBase/Config.js
@@ -1,2 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/E-Commerce");
+// mongoose.connect("mongodb://localhost:27017/E-Commerce");
+mongoose.connect(
+  "mongodb+srv://Arnab199993:Arnab123....@cluster0.symytbj.mongodb.net/E-Commerce"
+);
